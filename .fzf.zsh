7cec4a45341650651c6902378754697dfb04b7c5
Justin Zhu <justinthezhu@gmail.com>
2021-01-03 12:50:28 -0700

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -17,10 +17,11 @@ export FZF_DEFAULT_OPTS="
 --preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
 --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
 --bind '?:toggle-preview'
---bind 'ctrl-a:select-all'
 --bind 'ctrl-y:execute-silent(echo {+} | pbcopy)'
 --bind 'ctrl-e:execute(echo {+} | xargs -o vim)'
 "
+
+# --bind 'ctrl-a:select-all'
 
 # export FZF_COMPLETION_TRIGGER="**"
 # export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
