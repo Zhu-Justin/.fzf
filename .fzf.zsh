0ac33e1eae8e33c717ec5c4fa773f0decc1ebc37
Justin Zhu <justinthezhu@gmail.com>
2021-01-03 12:57:26 -0700

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -12,6 +12,7 @@ fi
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
+# export FZF_DEFAULT_COMMAND="find --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
 
 export FZF_DEFAULT_OPTS="
 --info=inline
