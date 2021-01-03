a3a94a8e09cbf2b4908eed5c44a177ffc4403d47
Justin Zhu <justinthezhu@gmail.com>
2021-01-03 12:45:25 -0700

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -11,3 +11,7 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_OPTS="--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'"
+# export FZF_COMPLETION_TRIGGER="**"
+# export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
