3420c26406044adf7aed28fa2fe79967fb15d1e4
Justin Zhu <justinthezhu@gmail.com>
2021-01-03 19:26:18 -0700

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -22,7 +22,7 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 # export FZF_DEFAULT_COMMAND="find . -name .git -prune -o -name node_modules -prune -o -name coverage -prune -o -name tmp -prune -not -path '*/[@.]*' -type f -print"
 
-export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,Downloads,Music,Movies,A,Sync} --type f"
+export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,Downloads,Music,Movies,A} --type f"
 
 # export FZF_DEFAULT_COMMAND='find . -name .git -prune -o -name node_modules -prune -o -name coverage -prune -o -name tmp -prune -type f -print'
 
