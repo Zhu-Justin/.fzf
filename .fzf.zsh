a25709169587dd12162f9e8946ca445c7b5b502f
Justin Zhu <justinthezhu@gmail.com>
2021-01-03 18:53:17 -0700

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -12,9 +12,11 @@ fi
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
-# export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
+# FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
 
-export FZF_DEFAULT_COMMAND='find . -name .git -prune -o -name node_modules -prune -o -name coverage -prune -o -name tmp -prune -o -prune -o -name .* prune -type f -print'
+export FZF_DEFAULT_COMMAND="find . -name .git -prune -o -name node_modules -prune -o -name coverage -prune -o -name tmp -prune -o -prune -not -name '.*' -print"
+
+# FZF_DEFAULT_COMMAND='find . -name .git -prune -o -name node_modules -prune -o -name coverage -prune -o -name tmp -prune -o -prune -not -name '.*' -type f -print'
 
 export FZF_DEFAULT_OPTS="
 --height=80%
