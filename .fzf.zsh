fe7886449c0144e62cd10845990c31f3a007fa1c
Justin Zhu <justinthezhu@gmail.com>
2021-02-08 14:31:32 -0500

    Update

--- .fzf.zsh
+++ .fzf.zsh
@@ -53,7 +53,7 @@ export FZF_DEFAULT_OPTS="
 # FZF_DEFAULT_COMMAND="find --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
 
 # FZF_DEFAULT_COMMAND="find ."
-# export FZF_COMPLETION_TRIGGER="**"
+export FZF_COMPLETION_TRIGGER="**"
 # export FZF_COMPLETION_TRIGGER=""
 # FZF_ALT_C_COMMAND="rg --sort-files --files --null 2> /dev/null | xargs -0 dirname | uniq".
 # FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
